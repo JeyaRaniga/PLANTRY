ae0d1a2a4cb21358b880697169c45fc06f26c5d6
Jeya Raniga <jayafj@gmail.com>
2019-11-14 18:12:17 +1300

    set up of boiler-plate

---
+++ Server/server.js
@@ -0,0 +1,14 @@
+const path = require('path')
+const express = require('express')
+const cors = require('cors')
+
+const server = express()
+const router = require('../Routes/routes')
+
+server.use(express.static(path.join(__dirname, '../Public')))
+server.use(express.json())
+server.use(cors())
+
+server.use('api/v1/', router)
+
+module.exports = server
